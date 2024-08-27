d403c3ac7f24bd7dfd4b20fa85606c335db18472
Gustavo Sanchez <gustavosanchezgalarza@gmail.com>
2024-08-26 22:03:52 -0500

    Create web component

--- components/SocialLinkProfile.js
+++ components/SocialLinkProfile.js
@@ -1,6 +1,21 @@
 export class SocialLinkProfile extends HTMLElement {
   constructor() {
     super();
+    const template = document.getElementById("social-link-profile-template");
+    const content = template.content.cloneNode(true);
+    const styles = document.createElement("style");
+
+    this.root = this.attachShadow({ mode: "open" });
+    this.root.appendChild(content);
+    this.root.appendChild(styles);
+
+    async function loadCSS() {
+      const request = await fetch("/components/SocialLinkProfile.css");
+      const css = await request.text();
+      styles.textContent = css;
+    }
+
+    loadCSS();
   }
 }
 
